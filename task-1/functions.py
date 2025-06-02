474739c1982145aade6a8ab11a0da30a1a8f9db8
TheProgrammingArchive <shrivatsant@gmail.com>
2025-06-02 11:08:33 +0530

    feat: Implemented MLP. Display accuracy on fashionmnist dataset, fix: critical issue with sigmoid function returning (z-1) instead of (1-z)

--- task-1/functions.py
+++ task-1/functions.py
@@ -1,7 +1,12 @@
 import numpy as np
 
-def sigmoid(z) -> np.float:
+def sigmoid(z):
     return 1.0/(1.0 + np.exp(-z))
 
-def sigmoid_prime(z) -> np.float:
-    return sigmoid(z)*(sigmoid(z) - 1)
+def sigmoid_prime(z):
+    return sigmoid(z)*(1 - sigmoid(z))
+
+def softmax(z):
+    z_shifted = z - np.max(z)
+    exp_z = np.exp(z_shifted)
+    return exp_z / np.sum(exp_z)
--- task-1/mlp.py
+++ task-1/mlp.py
@@ -1,85 +1,123 @@
 import numpy as np
-from functions import *
 import random
+from functions import *
+import copy
 
 class MLP:
-    def __init__(self, layers):
-        self.layers = layers
+    def __init__(self, layers: list, output_activation: str):
+        '''
+            layers: Provide number of neurons per layer in form of a list, [input_layers, hidden1, hidden2, ..., hiddenk, output]
+            output_activation: Change activation function depending on task (classification/regression), optimal loss function is chosen automatically
+        '''
         self.layer_count = len(layers)
-        self.weights, self.biases = [None for _ in range(self.layer_count - 1)], [None for _ in range(self.layer_count - 1)]
-        for k in range(1, self.layer_count):
-            self.weights[k - 1] = np.random.randn(self.layers[k], self.layers[k - 1])
-            self.biases[k - 1] = np.random.randn(self.layers[k], 1)
+        self.hidden_layers = self.layer_count - 1
+        self.layers = layers
+        self.output_activation = output_activation
+        
+        # Randomly initialize weights
+        self.weights = [np.random.randn(y, x) * np.sqrt(2.0 / (x + y))
+                        for x, y in zip(layers[:-1], layers[1:])]
+        self.biases = [np.random.randn(y, 1) for y in layers[1: ]]
 
-    def feed_forward(self, activation) -> list:
-        '''
-            Feed data through the layers, activation of layer l = w(l).a(l - 1) + b(l)
-            Returns the zvectors and activations for each layer
-        '''
-        z_vectors, activations = [], [activation]
-        for k in range(self.layer_count - 1):
-            z_vectors.append(np.dot(self.weights[k], activation[k]) + self.bias[k])
-            activation = sigmoid(np.dot(self.weights[k], activation[k]) + self.bias[k])
+
+    def feed_forward(self, activation):
+        z_vectors = []
+        activations = [activation]
+        current_layer = 0
+
+        for weight, bias in zip(self.weights, self.biases):
+            z_vector = np.dot(weight, activation) + bias
+            if current_layer == self.hidden_layers:
+                if self.output_activation == 'sigmoid':
+                    activation = sigmoid(z_vector)
+
+                elif self.output_activation == 'softmax':
+                    activation = softmax(z_vector)
+
+                else:
+                    activation = z_vector
+
+            else:
+                activation = sigmoid(z_vector)
+
+            current_layer += 1
+            z_vectors.append(z_vector)
             activations.append(activation)
 
         return z_vectors, activations
     
-    def SGD(self, train_data: list, n_epochs, batch_size, learning_rate, validation_data=None, early_stop_params: tuple = None):
-        for iterations in range(n_epochs):
+
+    def update_weights(self, batch, learning_rate):
+        weight_update = [np.zeros(w.shape) for w in self.weights]
+        bias_update = [np.zeros(b.shape) for b in self.biases]
+        for X, y in batch:
+            grad_c_wrt_bias, grad_c_wrt_weights = self.back_propagate(X, y)
+            weight_update = [wu + du for wu, du in zip(weight_update, grad_c_wrt_weights)]
+            bias_update = [bu + du for bu, du in zip(bias_update, grad_c_wrt_bias)]
+
+        self.weights = [weight - (learning_rate/len(batch))*update for weight, update in zip(self.weights, weight_update)]
+        self.biases = [bias - (learning_rate/len(batch))*update for bias, update in zip(self.biases, bias_update)]
+
+    def back_propagate(self, x, y):
+        z_vectors, activations = self.feed_forward(x)
+        if self.output_activation == 'sigmoid':
+            # MSE loss
+            delta = (activations[-1] - y)*sigmoid_prime(z_vectors[-1])
+        elif self.output_activation == 'softmax':
+            # Assumes cross-entropy loss
+            delta = (activations[-1] - y) 
+        else:
+            # MSE loss
+            delta = (activations[-1] - y)
+        
+        grad_c_wrt_weights = [np.zeros(w.shape) for w in self.weights]
+        grad_c_wrt_bias = [np.zeros(b.shape) for b in self.biases]
+
+        grad_c_wrt_bias[-1] = delta
+        grad_c_wrt_weights[-1] = np.outer(delta, activations[-2].T)
+
+        for layer in range(2, self.layer_count):
+            delta = np.dot(self.weights[-layer + 1].transpose(), delta) * sigmoid_prime(z_vectors[-layer])
+            grad_c_wrt_bias[-layer] = delta
+            grad_c_wrt_weights[-layer] = np.outer(delta, activations[-layer - 1].T)
+
+        return grad_c_wrt_bias, grad_c_wrt_weights
+    
+
+    def SGD(self, train_data, n_epochs, learning_rate, batch_size=16, validation_data=None, early_stop_patience: int=None):
+        '''
+        SGD optimizer on mini-batches to update weights and biases
+        '''
+        for epoch in range(n_epochs):
             random.shuffle(train_data)
             batches = []
             for k in range(0, len(train_data), batch_size):
                 batches.append(train_data[k: k + batch_size])
 
             for batch in batches:
-                bias_grads, weight_grads = self.back_propagate(batch[0], batch[1])
-                for k in range(len(weight_grads)):
-                    self.weights[k] = self.weights[k] - (learning_rate/batch_size)*weight_grads[k]
-                    self.biases[k] = self.biases[k] - (learning_rate/batch_size)*bias_grads[k]
+                self.update_weights(batch, learning_rate)
 
-            best_acc = 0
-            if early_stop_params is not None and validation_data is not None:
-                patience = early_stop_params[1]
-                accuracy = self.evaluate(validation_data)/len(validation_data)
-                best_acc = max(accuracy, best_acc)
-                cntr = 0
-                if accuracy < best_acc:
-                    cntr += 1
-                    if cntr == patience:
-                        return self.weights, self.biases
+            if validation_data:
+                print(f'Epoch: {epoch}| Result: {self.evaluate(validation_data)}/{len(validation_data)}')
+            else:
+                print(f'Epoch: {epoch} complete!')
+
+        #To-implement: early stopping 
+
+    def predict(self, data):
+        if self.output_activation == 'softmax' or self.output_activation == 'sigmoid':
+            test_results = self.feed_forward(data[0])[1][-1]
+            return np.argmax(test_results)
         
-            print(f'Epoch {iterations}')
+        
+    def fit_model(self, train_data, n_epochs, learning_rate, batch_size=16, validation_data=None, early_stop_patience: int=None):
+        self.SGD(train_data=train_data, n_epochs=n_epochs, learning_rate=learning_rate, batch_size=batch_size, validation_data=validation_data, early_stop_patience=early_stop_patience)
 
-    def back_propagate(self, x, y):
-        cost_grad_wrt_bias = [np.zeros(bias.shape) for bias in self.biases]
-        cost_grad_wrt_weights = [np.zeros(weights.shape) for weights in self.weights]  
 
-        z_vectors, activations = self.feed_forward(x)
-        activations.insert(0, x)
+    def evaluate(self, validation_data):
+        if self.output_activation == 'softmax' or self.output_activation == 'sigmoid':
+            test_results = [(np.argmax(self.feed_forward(x)[1][-1]), np.argmax(y))
+                        for (x, y) in validation_data]
+            return sum(int(x == y) for (x, y) in test_results)
 
-        last_delta = np.multiply(self.cost_derivative_wrt_activations(activations[-1], y), sigmoid_prime(z_vectors[-1]))
-        cost_grad_wrt_bias[-1] = last_delta
-        cost_grad_wrt_weights[-1] = np.dot(last_delta, activations[-2].transpose())
-
-        for l in range(2, self.layer_count):
-            l_delta = np.multiply(np.dot(self.weights[-l + 1], last_delta), sigmoid_prime(z_vectors[-l]))
-            last_delta = l_delta
-            cost_grad_wrt_bias[-l] = l_delta
-            cost_grad_wrt_weights[-l] = np.dot(l_delta, activations[-l - 1].transpose())
-
-        return cost_grad_wrt_bias, cost_grad_wrt_weights
-    
-    def evaluate(self, data_):
-        res = [(np.argmax(self.feed_forward(data[0])[1]), data[1]) for data in data_]
-        corr = 0
-        for z in res:
-            if z[0] == z[1]:
-                corr += 1
-
-        return corr
-
-    def cost_derivative_wrt_activations(self, a, y):
-        return a - y
-
-net = MLP([2, 4, 1])
-net.print_weights()
+        # To-implement: evaluate for regression
