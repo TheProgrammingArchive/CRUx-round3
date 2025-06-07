877fa2d2ace18c0b6c1b1b5c2671796127923147
TheProgrammingArchive <shrivatsant@gmail.com>
2025-06-07 21:31:44 +0530

    Almost done implementing live plotting

--- task-1/liveplot.py
+++ task-1/liveplot.py
@@ -1,22 +1,56 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-plt.style.use('fivethirtyeight')
+from datetime import datetime
+from matplotlib import pyplot
+from matplotlib.animation import FuncAnimation
+from random import randrange
 
-def animate(i):
-    f = open('dataw.txt', 'r')
-    data = f.readlines()[-1]
-    y1 = data.split(',')[1]
-    y2 = data.split(',')[2]
 
-    print(data)
+import numpy as np
+import keras
+from mlp import MLP, EarlyStopping, grid_search
+from sklearn.linear_model import SGDClassifier
 
-    plt.cla()
-    plt.plot(data.split(',')[0], y1, label='Acc')
-    plt.plot(data.split(',')[0], y2, label='Loss')
+def prepare_data(data):
+    X, y = data
+    X = X.astype('float64')/255.
+    X = [np.reshape(x, (784, 1)) for x in X]
 
-    plt.legend(loc='upper left')
-    plt.tight_layout()
+    y_fin = [None for k in range(len(X))]
+    for idx, y_ in enumerate(y):
+        y_vec = np.zeros((10, 1))
+        y_vec[y_] = 1
+        y_fin[idx] = y_vec
 
-ani = animation.FuncAnimation(plt.gcf(), animate, interval=10000, save_count=10000)
-plt.tight_layout()
-plt.show()
+    return list(zip(X, y_fin))
+
+
+data = keras.datasets.fashion_mnist.load_data()
+train_data, test_data = data
+
+train_data = prepare_data(train_data)
+test_data = prepare_data(test_data)
+
+# Custom-model
+model = MLP([784, 128, 64, 10])
+
+x_data, y_data, y1_data = [], [], []
+
+figure, ax = pyplot.subplots()
+line, = ax.plot(x_data, y_data, '-', label='loss')
+liner, = ax.plot(x_data, y1_data, '--', label='accuracy')
+
+def update(frame):
+    l, a = model.fit(train_data=train_data, n_epochs=1, learning_rate=0.1, batch_size=32, validation_data=test_data[:2500])
+    x_data.append(len(x_data))
+    y_data.append(l[-1])
+    y1_data.append(a[-1])
+
+    line.set_data(x_data, y_data)
+    liner.set_data(x_data, y1_data)
+
+    ax.relim()
+    ax.autoscale_view()
+    return line, liner
+
+animation = FuncAnimation(figure, update, interval=200, cache_frame_data=False)
+
+pyplot.show()
