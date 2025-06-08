6d4a16f9de1ba13f8ca79652644e531eed429191
TheProgrammingArchive <shrivatsant@gmail.com>
2025-06-08 19:47:28 +0530

    Add liveplot as a function

--- task-1/liveplot.py
+++ task-1/liveplot.py
@@ -30,27 +30,38 @@ train_data = prepare_data(train_data)
 test_data = prepare_data(test_data)
 
 # Custom-model
-model = MLP([784, 128, 64, 10])
+modelr = MLP([784, 128, 64, 10])
 
-x_data, y_data, y1_data = [], [], []
+def test(model, train_data, n_epochs, learning_rate, batch_size, validation_data):
+    x_data, y_data, y1_data = [], [], []
 
-figure, ax = pyplot.subplots()
-line, = ax.plot(x_data, y_data, '-', label='loss')
-liner, = ax.plot(x_data, y1_data, '--', label='accuracy')
+    figure, ax = pyplot.subplots()
+    line, = ax.plot(x_data, y_data, '-', label='loss')
+    liner, = ax.plot(x_data, y1_data, '--', label='accuracy')
 
-def update(frame):
-    l, a = model.fit(train_data=train_data, n_epochs=1, learning_rate=0.1, batch_size=32, validation_data=test_data[:2500])
-    x_data.append(len(x_data))
-    y_data.append(l[-1])
-    y1_data.append(a[-1])
+    assert(n_epochs >= 0)
 
-    line.set_data(x_data, y_data)
-    liner.set_data(x_data, y1_data)
+    def update(frame):
+        l, a = model.fit(train_data=train_data, n_epochs=1, learning_rate=learning_rate, batch_size=batch_size, validation_data=validation_data)
+        x_data.append(len(x_data))
+        y_data.append(l[-1])
+        y1_data.append(a[-1])
 
-    ax.relim()
-    ax.autoscale_view()
-    return line, liner
+        line.set_data(x_data, y_data)
+        liner.set_data(x_data, y1_data)
 
-animation = FuncAnimation(figure, update, interval=200, cache_frame_data=False)
+        ax.relim()
+        ax.autoscale_view()
 
-pyplot.show()
+        if len(x_data) == n_epochs:
+            animation.event_source.stop()
+            x = input('Training complete, press any key to terminate plot')
+            pyplot.close(figure)
+
+        return line, liner
+
+    animation = FuncAnimation(figure, update, interval=200, cache_frame_data=False)
+
+    pyplot.show()
+
+test(modelr, train_data=train_data, n_epochs=3, learning_rate=0.1, batch_size=16, validation_data=test_data[:1000])
