d497c8ba52886c18392609b5f1b1db10cd27bff1
TheProgrammingArchive <shrivatsant@gmail.com>
2025-06-09 10:40:04 +0530

    Add few more examples

--- task-1/main.py
+++ task-1/main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import keras
 from mlp import MLP, EarlyStopping, grid_search
-from sklearn.linear_model import SGDClassifier
 from liveplot import liveplot
 
 def prepare_data(data):
@@ -24,17 +23,19 @@ train_data, test_data = data
 train_data = prepare_data(train_data)
 test_data = prepare_data(test_data)
 
-# # Custom-model, Learning with no graph
-# model = MLP([784, 128, 64, 10])
-# estop = EarlyStopping(monitor='loss', patience=2, restore_best_weights=True)
-# l, a = model.fit(train_data=train_data, n_epochs=10, learning_rate=0.1, batch_size=32, validation_data=test_data[:2500], early_stop=estop)
+# Custom-model, Learning with no plot
+model = MLP([784, 128, 64, 10])
+estop = EarlyStopping(monitor='acc', patience=2, restore_best_weights=True)
+print(train_data[0][0].shape)
+l, a = model.fit(train_data=train_data, n_epochs=10, learning_rate=0.1, batch_size=32, validation_data=test_data[:2500], early_stop=estop)
+print(model.predict(test_data[0][0]))
 
-# # Custom-model, Learning with graph
+# # Custom-model, Learning with plot
 # model = MLP([784, 128, 64, 10])
 # liveplot(model, train_data=train_data, validation_data=test_data, learning_rate=0.1, batch_size=32, n_epochs=5)
 
-
+# # Obtain best model with grid search
 # best_model, best_params = grid_search(train_data=train_data, test_data=test_data, param_grid={'layers': [[784, 32, 10], [784, 128, 64, 10]], 'n_epochs': [10], 'learning_rate': [1, 0.1], 'batch_size': [16, 32]})
 
 
-# Best accuracy 0.8841 using grid search
+# Best accuracy 0.8941 using grid search
