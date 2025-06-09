6efd5b7184189979a7f0ed27957a5c235c2aa5da
TheProgrammingArchive <shrivatsant@gmail.com>
2025-06-09 11:18:42 +0530

    feat: Update docs

--- task-1/main.py
+++ task-1/main.py
@@ -26,9 +26,7 @@ test_data = prepare_data(test_data)
 # Custom-model, Learning with no plot
 model = MLP([784, 128, 64, 10])
 estop = EarlyStopping(monitor='acc', patience=2, restore_best_weights=True)
-print(train_data[0][0].shape)
 l, a = model.fit(train_data=train_data, n_epochs=10, learning_rate=0.1, batch_size=32, validation_data=test_data[:2500], early_stop=estop)
-print(model.predict(test_data[0][0]))
 
 # # Custom-model, Learning with plot
 # model = MLP([784, 128, 64, 10])
@@ -36,6 +34,3 @@ print(model.predict(test_data[0][0]))
 
 # # Obtain best model with grid search
 # best_model, best_params = grid_search(train_data=train_data, test_data=test_data, param_grid={'layers': [[784, 32, 10], [784, 128, 64, 10]], 'n_epochs': [10], 'learning_rate': [1, 0.1], 'batch_size': [16, 32]})
-
-
-# Best accuracy 0.8941 using grid search
